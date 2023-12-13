1d8dda5b9a6cb72f93b288d187403eac11bb55f3
Hanz <hanz@godot.id>
2023-12-13 13:57:05 +0700

    feat: Added Basic toolbox

---
+++ Toolbox.gd
@@ -0,0 +1,31 @@
+extends HBoxContainer
+
+var tools = {
+	"Select": "Click on an element to select them",
+	"Element": "",
+	"Move": "",
+	"Save": "",
+	"Load": "Load a file"
+}
+
+func _ready():
+	$SelectButton.button_group.connect("pressed", __on_button_pressed)
+	for button in $SelectButton.button_group.get_buttons():
+		button.connect("mouse_entered", __change_tool_desc_button.bind(button))
+	$"..".connect("mouse_exited", __change_tool_desc_active)
+	__change_tool_desc_active()
+
+func __change_tool_desc_active():
+	var button = $SelectButton.button_group.get_pressed_button()
+	
+	var selected = button.name.replace("Button", "")
+	$"../../VBoxContainer/Title".text = selected
+	$"../../VBoxContainer/Desc".text = tools[selected]
+
+func __change_tool_desc_button(button: BaseButton):
+	var selected = button.name.replace("Button", "")
+	$"../../VBoxContainer/Title".text = selected
+	$"../../VBoxContainer/Desc".text = tools[selected]
+
+func __on_button_pressed(button: BaseButton):
+	__change_tool_desc_button(button)
